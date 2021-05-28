2401635e064f0e854e519589d43f24c47f7880e2
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2021-05-29 03:03:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 # Print results.
 for row in rows:
-  st.write(f"{row.Name} has a :{int(row.Group_number)}:")
+  st.write(f"{row.Name} has a : {int(row.Group_number)}:")
 
 
