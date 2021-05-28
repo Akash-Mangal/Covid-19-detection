af2dc1895c01f26aa1873aab851dc64b3f0482a6
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2021-05-29 04:45:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ img = pyqrcode.create(s)
 # Create and save the svg file naming “myqr.svg”
 #img.svg(“myqr.svg”, scale = 8)
 # Create and save the png file naming “myqr.png”
-img.png(‘myqr.png’, scale = 6)
-st.image(img)
+#img.png(‘myqr.png’, scale = 6)
+image = Image.open(io.BytesIO(img))
+st.image(image)
 
 
