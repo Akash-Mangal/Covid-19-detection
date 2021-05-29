d25f83461c3059192e7c0fd85a521e7638d20580
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2021-05-29 16:37:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ all_memb= st.button("Show all data")
 if all_memb:
   rows = run_query(f'SELECT * FROM "{sheet_url}"')
   for row in rows:
-    st.write(f"{row.Name} is in group number: ")
+   st.multiselect(f"{row.Name} is in group number: ")
  
-
- 
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+st.multiselect(rows.Name)
 
 
